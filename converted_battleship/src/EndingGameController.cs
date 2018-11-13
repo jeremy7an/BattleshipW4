673bb1720d86f38c63e5a1cdb4795c61747b3132
Alibek Maratov <alibekKZ3@gmail.com>
2018-11-14 00:23:58 +0800

    The result will be shown in the message box
    The final output will be shown in a new window

--- converted_battleship/src/EndingGameController.cs
+++ converted_battleship/src/EndingGameController.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using SwinGameSDK;
+using System.Windows.Forms;
 
 /// <summary>
 /// The EndingGameController is responsible for managing the interactions at the end
@@ -30,18 +31,14 @@ static class EndingGameController
 
 		} else {
 
-			if (GameController.ComputerPlayer.PlayerGrid.ShipsKilled == 4 || GameController.HumanPlayer.PlayerGrid.ShipsKilled == 4) {
-
-				SwinGame.DrawTextLines ("You have only one ship left!", Color.White, Color.Transparent, GameResources.GameFont ("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth (), SwinGame.ScreenHeight ());
-
-			}
-
 			if (GameController.HumanPlayer.IsDestroyed) {
-				SwinGame.DrawTextLines ("YOU LOSE!", Color.White, Color.Transparent, GameResources.GameFont ("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth (), SwinGame.ScreenHeight ());
-				SwinGame.DrawTextLines ("PRESS ESCAPE TO PROCEED", Color.White, Color.Transparent, GameResources.GameFont ("Courier"), FontAlignment.AlignCenter, 0, 350, SwinGame.ScreenWidth (), SwinGame.ScreenHeight ());
+				MessageBox.Show ("It is ok, try next time!"+"\nPress (ok) to continue ","Try again");
+				HighScoreController.ReadHighScore (GameController.HumanPlayer.Score);
+				GameController.EndCurrentState ();
 			} else {
-				SwinGame.DrawTextLines ("-- WINNER --", Color.White, Color.Transparent, GameResources.GameFont ("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth (), SwinGame.ScreenHeight ());
-				SwinGame.DrawTextLines ("PRESS ESCAPE TO PROCEED", Color.White, Color.Transparent, GameResources.GameFont ("Courier"), FontAlignment.AlignCenter, 0, 350, SwinGame.ScreenWidth (), SwinGame.ScreenHeight ());
+				MessageBox.Show ("You win, woooohoooo!"+"\nPress (ok) to continue ","Congratulations!");
+				GameController.EndCurrentState ();
+				HighScoreController.ReadHighScore (GameController.HumanPlayer.Score);
 			}
 		}
 	}
@@ -53,7 +50,7 @@ static class EndingGameController
 	public static void HandleEndOfGameInput()
 	{
 		//press escape to continue instead of any key so that msg wont be skipped accidently by user - added by Jeremy Toh
-		if (SwinGame.KeyTyped(KeyCode.vk_ESCAPE)) {
+		if (SwinGame.KeyTyped(KeyCode.vk_ESCAPE)||SwinGame.MouseClicked(MouseButton.LeftButton)) {
 			GameResources.Unmute (); //resume music after loser/winner sound effect - added by Jeremy Toh
 			HighScoreController.ReadHighScore(GameController.HumanPlayer.Score);
 			GameController.EndCurrentState();
